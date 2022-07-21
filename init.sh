82a47117939f05daabb5a40dd54171679b0c21fc
Paul Bargewell <paul.bargewell@opusvl.com>
2022-07-21 10:56:24 +0100

    init-db.sh permissions

--- init.sh
+++ init.sh
@@ -22,7 +22,7 @@ fi
 source .env
 
 docker-compose build && \
-chmod ugo+x init-db.sh && \
+chmod ugo+rx init-db.sh && \
 docker-compose up -d db
 
 docker-compose run --rm -u root odoo chown odoo: /etc/odoo /var/lib/odoo /mnt/extra-addons -Rv
